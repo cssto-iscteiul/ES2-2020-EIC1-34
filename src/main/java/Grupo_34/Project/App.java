c09fd8a72052ec91803ade7b768020d9d6188c28
Catarina Teodoro <cssto@iscte-iul.pt>
2020-03-24 15:41:00 +0000

    First commit

---
+++ src/main/java/Grupo_34/Project/App.java
@@ -0,0 +1,13 @@
+package Grupo_34.Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
