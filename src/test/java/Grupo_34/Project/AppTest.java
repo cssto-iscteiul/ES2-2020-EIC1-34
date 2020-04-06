7cda27abcc2eb2f36222f19cfe3d5f65516dcc0f
jptmo1 <jptmo1@iscte-iul.pt>
2020-04-06 17:58:56 +0100

    alterado

--- src/test/java/Grupo_34/Project/AppTest.java
+++ src/test/java/Grupo_34/Project/AppTest.java
@@ -1,8 +1,9 @@
 package Grupo_34.Project;
 
-import junit.framework.Test;
+//import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -10,27 +11,28 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
     	App junit= new App();
